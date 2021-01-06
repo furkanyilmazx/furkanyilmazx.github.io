095b710f1c38216d5ec4a465e452843bc600cd20
Furkan YILMAZ <furkanyilmazx@gmail.com>
2021-01-06 17:36:51 +0300

    Updates

---
+++ precache-manifest.2770c20aaff32c56e959fbe9a176e3fc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7696e9ff182d9fc51dec9d5136ea13cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "310d44d8819a908eb558",
+    "url": "/static/css/main.3371461e.chunk.css"
+  },
+  {
+    "revision": "cced10ba3c22a2894b6b",
+    "url": "/static/js/2.27f73eef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27f73eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df9fa7734734cf0ef6c",
+    "url": "/static/js/3.d6e6e071.chunk.js"
+  },
+  {
+    "revision": "310d44d8819a908eb558",
+    "url": "/static/js/main.50466eb2.chunk.js"
+  },
+  {
+    "revision": "0dbbce3811f1e55a6ece",
+    "url": "/static/js/runtime-main.8e7b6f6a.js"
+  },
+  {
+    "revision": "1e1cdbceb410da1734e513cb373f875b",
+    "url": "/static/media/GB-Flag.1e1cdbce.svg"
+  },
+  {
+    "revision": "117745cc81c9c893a23436015841fab0",
+    "url": "/static/media/ProfileImage.117745cc.png"
+  },
+  {
+    "revision": "0ed48840c0779ffb1acd55aaec0e2cbf",
+    "url": "/static/media/TR-Flag.0ed48840.svg"
+  },
+  {
+    "revision": "3522402523e6d1eb2bd53f458fd5fa5e",
+    "url": "/static/media/business-icon.35224025.svg"
+  },
+  {
+    "revision": "ec2b9c8f4d8446957bd988ac9a8b3d49",
+    "url": "/static/media/date-icon.ec2b9c8f.svg"
+  },
+  {
+    "revision": "eb1cb22d6e93f40408f673ffed66d54f",
+    "url": "/static/media/dribble.eb1cb22d.svg"
+  },
+  {
+    "revision": "028cccf7b3729042d72937b9778e9400",
+    "url": "/static/media/education-icon.028cccf7.svg"
+  },
+  {
+    "revision": "8c102b4d8af7dfb0a16aa4fadf5aa4c6",
+    "url": "/static/media/email-icon.8c102b4d.svg"
+  },
+  {
+    "revision": "f8a2c2343f7cd38d21082d5a96df39f6",
+    "url": "/static/media/github-icon.f8a2c234.svg"
+  },
+  {
+    "revision": "3cb701481f934423ec585f7523bf176d",
+    "url": "/static/media/group-icon.3cb70148.svg"
+  },
+  {
+    "revision": "b8569be708dde8eb0aef172973e927e3",
+    "url": "/static/media/instagram-icon.b8569be7.svg"
+  },
+  {
+    "revision": "515015f1f669170bc1e9f3dcf26e4e75",
+    "url": "/static/media/launch-icon.515015f1.svg"
+  },
+  {
+    "revision": "70650012f7e73995484c4fabb6ea6236",
+    "url": "/static/media/linkedin-icon.70650012.svg"
+  },
+  {
+    "revision": "935860278200f752c2fbc4296d78d100",
+    "url": "/static/media/location-icon.93586027.svg"
+  },
+  {
+    "revision": "bad7a003c8e17e6d26df389262389924",
+    "url": "/static/media/resume-pdf.bad7a003.pdf"
+  },
+  {
+    "revision": "aaad37f51a62de8e07653d08d9295991",
+    "url": "/static/media/work-icon.aaad37f5.svg"
+  },
+  {
+    "revision": "e8de0806af1cfa9e06754e9e3d7334a8",
+    "url": "/static/media/world-icon.e8de0806.svg"
+  }
+]);
